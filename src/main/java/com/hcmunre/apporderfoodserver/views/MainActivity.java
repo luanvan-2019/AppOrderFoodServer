549e17c1f9a5aef116f100e6ad583adcddf17a7d
huutrong1097 <huutrong1097@users.noreply.github.com>
2019-09-06 14:14:51 +0700

    first commit

---
+++ src/main/java/com/hcmunre/apporderfoodserver/views/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hcmunre.apporderfoodserver.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hcmunre.apporderfoodserver.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
